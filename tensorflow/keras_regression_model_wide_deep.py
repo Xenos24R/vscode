da574cc29c9529a8f9aff06d91658d504e906ac1
Xenos24R <329365032@qq.com>
2020-04-06 13:04:53 +0800

    Xenos

---
+++ tensorflow/keras_regression_model_wide_deep.py
@@ -0,0 +1,68 @@
+import os
+os.environ["CUDA_VISIBLE_DEVICES"] = "-1"#强制使用CPU
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import numpy as np
+import sklearn
+import pandas as pd
+import sys
+import time
+import tensorflow as tf
+
+from sklearn.datasets import fetch_california_housing
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from tensorflow import keras
+
+import pprint
+def plot_learning_curves(history):
+    pd.DataFrame(history.history).plot(figsize=(8,5))
+    plt.grid(True)
+    plt.gca().set_ylim(0,1)
+    plt.show()
+
+housing = fetch_california_housing()
+
+print(housing.DESCR)
+print(housing.data.shape)
+print(housing.target.shape)
+pprint.pprint(housing.data[0:5])
+pprint.pprint(housing.target[0:5])
+
+#分割训练集和测试集
+x_train_all,x_test,y_train_all,y_test = train_test_split(
+    housing.data,housing.target,random_state=7
+)
+#分割验证集和训练集
+x_train,x_valid,y_train,y_valid = train_test_split(
+    x_train_all,y_train_all,random_state=11
+)
+
+#print(x_train.shape,y_train.shape)
+#print(x_valid.shape,y_valid.shape)
+#print(x_test.shape,y_test.shape)
+
+#归一化
+scaler = StandardScaler()
+x_train_scaled = scaler.fit_transform(x_train)
+x_valid_scaled = scaler.transform(x_valid)
+x_test_scaled = scaler.transform(x_test)
+
+#函数式API
+input = keras.layers.Input(shape=x_train.shape[1:])
+#deep模型,此处未找到合适的激活函数
+hidden1 = keras.layers.Dense(30,activation='relu')(input)
+hidden2 = keras.layers.Dense(30,activation='relu')(hidden1)
+
+concat = keras.layers.concatenate([input,hidden2])
+output = keras.layers.Dense(1)(concat)
+
+model = keras.models.Model(inputs=[input],outputs=[output])
+
+model.summary()
+model.compile(loss='mean_squared_error',optimizer="sgd")#sgd梯度下降
+callbacks = [
+    keras.callbacks.EarlyStopping(patience=5,min_delta=1e-4)
+]
+history = model.fit(x_train_scaled,y_train,validation_data=(x_valid_scaled,y_valid),epochs=100,callbacks=callbacks)
+plot_learning_curves(history)
