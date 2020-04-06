1c8468bfbb8d5b982ab61b6461a4c0b15a7ffd10
Xenos24R <329365032@qq.com>
2020-04-06 09:32:32 +0800

    Xenos

--- opencv/image_demo.py
+++ opencv/image_demo.py
@@ -7,9 +7,10 @@ def get_image_info(image):
     print(image.size)
     print(image.dtype)
 
-src = cv.imread("C:/Users/32936/Desktop/2/1.jpg")
+src = cv.imread("C:/Users/32936/Desktop/2/lena.png")
 cv.namedWindow("input image",cv.WINDOW_AUTOSIZE)
 get_image_info(src)
 cv.imshow("input image",src)
-cv.waitKey(0)
+while(cv.waitKey(25)!=27):#cv.waitKey返回键入的ASCII码
+    pass
 cv.destroyAllWindows()
